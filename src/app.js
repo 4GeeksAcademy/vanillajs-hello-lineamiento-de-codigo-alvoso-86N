a9b91dbf81ef10941273367f5580d22b187171b9
Yoalvarado25 <manacima2019@gmail.com>
2025-07-26 14:42:08 +0000

    primer commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let first= "Ooh no!!!";
+
+  let who = ['The cat ', 'My grandma  ', 'The mailman  ', 'My bird  '];
+  let action = ['ate  ', 'peed  ', 'crushed  ', 'broke  '];
+  let what = ['my homework  ', 'my pc  ', 'the car  '];
+  let when = ['before the class  ', 'when I was sleeping  ', 'while I was exercising  ', 'during my lunch  ', 'while I was praying  ']; 
+
+  let rdm1 = Math.floor(Math.random() * who.length);
+  let rdm2 = Math.floor(Math.random() * action.length);
+  let rdm3 = Math.floor(Math.random() * what.length);
+  let rdm4 = Math.floor(Math.random() * when.length);
+
+  document.querySelector("#excuse").innerHTML = first + who[rdm1] + action[rdm2] + what[rdm3] + when[rdm4];
+  
+}
